ff0bdad98889644de765b4158a4442a414486f5d
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-25 11:59:53 +0530

    Update items_order.py

--- items_order.py
+++ items_order.py
@@ -1,4 +1,7 @@
 print("Welcome to Friend's Kitchen!")
+
+print("===================================")
+
 print("Here is the menu:")
 print("1. Spring Rolls |", "Rs. 39 |", "2 Nos")
 print("2. Dosa |", "Rs. 24 |", "10 Nos")
@@ -7,3 +10,6 @@ print("3. Dimsons |", "Rs. 49 |", "12 Nos")
 print (input("Enter your choice:"))
 
 print("Thank you for your order!\n It will be ready in 10 minutes.")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("End of Program")
